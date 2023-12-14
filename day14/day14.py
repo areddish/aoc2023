d73397786013d6a376a4a8e39c391e422447eeb8
Andrew Reddish <areddish@foosion.net>
2023-12-14 12:12:04 -0500

    Day 14 cleanup

--- day14/day14.py
+++ day14/day14.py
@@ -3,69 +3,60 @@ from collections import defaultdict
 #   Submission helper, print the answer and copy it to the clipboard
 #   to reduce the amount of times I have the answer and mistype it :).
 ###
-#import pyperclip
+import pyperclip
 answer_part = 1
 def answer(v):
     global answer_part
-    #pyperclip.copy(v)
+    pyperclip.copy(v)
     print("Part 1 =" if answer_part == 1 else "Part 2 =", v)
     answer_part = 2
 
 part1 = 0
 part2 = 0
 
-def p(board):
+def print_board(board):
     for y in range(len(board)):
         for x in range(len(board[0])):
             print(board[y][x], end="")
         print()
     
-def slide(board, x, y, dir):
+def slide(board, x, y, dir, W, H):
     dx,dy = dir
     if dy != 0:
-        while 0 <= y < len(board):
+        while 0 <= y < H:
             y += dy
-            if y < len(board) and board[y][x] != ".":
+            if y < H and board[y][x] != ".":
                 y -= dy
                 break
         if y < 0:
             y = 0
-        if y == len(board):
-            y = len(board) - 1
+        if y == H:
+            y = H - 1
     if dx != 0:
-        while 0 <= x < len(board[0]):
+        while 0 <= x < W:
             x += dx
-            if x < len(board[0]) and board[y][x] != ".":
+            if x < W and board[y][x] != ".":
                 x -= dx
                 break
         if x < 0:
             x = 0
-        if x == len(board[0]):
-            x = len(board[0]) - 1
+        if x == W:
+            x = W - 1
 
     return (x,y)
 
-def tilt(board, yrange, xrange, offset=(0,-1)):
-#    ans = sum([1 if ch == 'O' else 0 for ch in board[0]]) * (len(board))
+def tilt(board, yrange, xrange, offset, W, H):
     for y in yrange:
         for x in xrange:
             if board[y][x] == "O":
-                nx,ny = slide(board,x,y,offset)
+                nx,ny = slide(board,x,y,offset, W, H)
                 board[y][x] = "."
                 board[ny][nx] = "O"
-                # if (x,y) != (nx,ny):
-                #     val = len(board) - ny
-                #     #print(f"{len(board)} {ny} {x},{y} -> {nx},{ny}  {val}")
-                #     ans += val
-                # else:
-                #     val = len(board) - y
-                #     ans += val
 
-def compute_load(board):
+def compute_load(board, W, H):
     ans = 0
-    H = len(board)
     for y in range(H):
-        for x in range(len(board[0])):
+        for x in range(W):
             if board[y][x] == "O":
                 ans += H - y
     return ans
@@ -79,8 +70,10 @@ with open("day14.txt") as file:
         line = line.strip()
         board.append(list(line))
 
-    part1_done = False
-    #p(board)
+    W = len(board[0])
+    H = len(board)
+
+    #print_board(board)
     answers = defaultdict(list)
     # This number found by testing
     #  1000 -> no answer
@@ -88,17 +81,17 @@ with open("day14.txt") as file:
     #  8000 -> 2 answers (99861, 99875)
     #  10000 -> 1 answer.. and it's common with other's
     for i in range(1,10000+1):#000000+1):
-        tilt(board, range(0,len(board)), range(0,len(board[0])), (0,-1))
-        if not part1_done:
-            answer(compute_load(board))
+        tilt(board, range(0,H), range(0,W), (0,-1), W, H)
+        if i == 1:
+            answer(compute_load(board, W, H))
             part1_done = True
-        tilt(board, range(0,len(board)), range(0,len(board[0])), (-1,0))
-        tilt(board, range(len(board)-1,-1,-1), range(0,len(board[0])), (0,1))
-        tilt(board, range(0,len(board)), range(len(board[0])-1, -1, -1), (1,0))
-        load = compute_load(board)
+        tilt(board, range(0,H), range(0,W), (-1,0), W, H)
+        tilt(board, range(H-1,-1,-1), range(0,W), (0,1), W, H)
+        tilt(board, range(0,H), range(W-1, -1, -1), (1,0), W, H)
+        load = compute_load(board, W, H)
         answers[load].append(i)
         if i % 1000 == 0:
-            print(f"{i}: {load}")
+            print(f"Processed {i}")
 
     for k in answers:
         # need enough samples
